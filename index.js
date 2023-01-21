0214e7eb8b22e0e0b60423e0b46afa7814981e56
Nathaniel Pendy <nathanielp2402@gmail.com>
2023-01-21 15:06:43 -0600

    readme + tweak

--- index.js
+++ index.js
@@ -6,33 +6,37 @@ const useEvent = fn => {
   return ref.current;
 };
 
-export default ({ initial }) => {
+export default ({ initial, trackChanges }) => {
   const [changes, _setChanges] = useState({});
   const [changeLog, setChangeLog] = useState([]);
   const [pointer, setPointer] = useState(0);
 
   const redo = useEvent(() => {
-    if (!changeLog[pointer + 1]) return;
+    if (!trackChanges || !changeLog[pointer + 1]) return;
     setChanges(changes => ({ ...changes, ...changeLog[pointer + 1] }));
     setPointer(pointer => ++pointer);
   });
 
   const undo = useEvent(() => {
-    if (!changeLog[pointer - 1]);
+    if (!trackChanges || !changeLog[pointer - 1]) return;
     setChanges(changes => ({ ...changes, ...changeLog[pointer - 1] }));
     setPointer(pointer => --pointer);
   });
 
   const setChanges = useEvent(changes => {
-    setPointer(0);
-    setChangeLog([]);
+    if (trackChanges) { 
+      setPointer(0);
+      setChangeLog([]);
+    }
     return _setChanges(changes);
   });
 
   const addChanges = useEvent(diff => {
     setChanges(changes => {
-      setChangeLog(log => [...log, diff]);
-      setPointer(pointer => ++pointer);
+      if (trackChanges) { 
+        setChangeLog(log => [...log.slice(0, pointer), diff]);
+        setPointer(pointer => ++pointer);
+      }
       return { ...changes, ...diff };
     });
   });
